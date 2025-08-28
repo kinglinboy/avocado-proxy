66e374d94e6f67b6e9a0c13723fe0b072ae4f00b
kinglinboy <390664384@qq.com>
2025-08-28 15:47:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,26 @@
 export default {
+	
+	
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-		url.hostname = 'avocado-bo-dev.weyatech.cn';
+
+		const avocado_admin_host = 'avocado-bo-dev.weyatech.cn';
+		const avocado_intake_host = 'avocado-customer-dev.weyatech.cn';
+		const avocado_customer_host = 'avocado-customer-dev.weyatech.cn';
+
+		const admin_host = ${env.admin_host};
+		const intake_host = ${env.admin_host};
+		const customer_host = ${env.customer_host};
+
+		const url_host = url.hostname
+		if(customer_host === url_host){
+			url.hostname = avocado_customer_host;
+		}else if(intake_host === url_host){
+			url.hostname = avocado_intake_host;
+		} else {
+			url.avocado_admin_host = avocado_intake_host;
+		}
+		
 		console.log(`path:${url.pathname}: search:${url.searchParams}`);
 		const newRequest = new Request(url, request);
 		return fetch(newRequest);
